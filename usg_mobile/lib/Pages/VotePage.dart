7b4d47f112776a3140be32850ed395fb54322f4e
gotagetfood <96447537+gotagetfood@users.noreply.github.com>
2023-11-20 09:16:49 -0700

    reads the array now

--- usg_mobile/lib/Pages/VotePage.dart
+++ usg_mobile/lib/Pages/VotePage.dart
@@ -9,7 +9,7 @@ class votePage extends StatefulWidget {
   State<votePage> createState() => _votePageState();
   } //build
 
-  class _votePageState extends State<votePage> {
+class _votePageState extends State<votePage> {
     String user = "someone@mail.com"; //set equal to the users email
     String creator = "chcolby@student.neumont.edu"; //i'm not sure what this is used for tbh
     Map<String, int> usersWhoVoted = {
@@ -20,13 +20,37 @@ class votePage extends StatefulWidget {
       'someone2@mail.com': 3, //make this match the user to dissable votes for the debug test
     };    //this is got to be a database list of all the people. i think the numbers are to show what people voted.
 
-    double option1 = 1.0;
+    double option1 = 0.0;
     double option2 = 0.0;
-    double option3 = 1.0;
-    double option4 = 3.0; //how many have voted that option before
+    double option3 = 0.0;
+    double option4 = 0.0; //how many have voted that option before
+
+    void setValues(Map<String, int> usersWhoVoted) {
+      option1 = 0.0;
+      option2 = 0.0;
+      option3 = 0.0;
+      option4 = 0.0;
+      usersWhoVoted.forEach((email, voted) {
+        switch (voted) {
+          case 0:
+            option1++;
+            break;
+          case 1:
+            option2++;
+            break;
+          case 2:
+            option3++;
+            break;
+          case 3:
+            option4++;
+            break;
+        }
+      });
+    }
 
     @override
     Widget build(BuildContext context) {
+      setValues(usersWhoVoted);
       return Scaffold(
         appBar: appBar(),
         body: body(),
@@ -61,7 +85,7 @@ class votePage extends StatefulWidget {
               height: 400,
               decoration: BoxDecoration(
                 border: Border.all(),
-                color: Colors.grey,
+                color: Color(0x62BBB8B8),
                 borderRadius: BorderRadius.circular(30),
               ),
               margin: const EdgeInsets.fromLTRB(20, 40, 20, 5),
@@ -78,9 +102,11 @@ class votePage extends StatefulWidget {
                 foregroundColor: Colors.black,
                 textStyle: const TextStyle(fontSize: 20)),
             onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context){
-                return const votedPage();
-              }));
+              debugPrint(option1.toString() + ' ' + option2.toString() + ' ' +  option3.toString() + ' ' + option4.toString());
+
+              // Navigator.push(context, MaterialPageRoute(builder: (context){
+              //   return const votedPage();
+              // }));
               //Code for Yes goes here
               debugPrint('Next');
             },
@@ -126,22 +152,22 @@ class votePage extends StatefulWidget {
         setState(() {
           usersWhoVoted[user] = choice;
         });
-        if (choice == 1) {
+        if (choice == 0) {
           setState(() {
             option1 += 1.0;
           });
         }
-        if (choice == 2) {
+        if (choice == 1) {
           setState(() {
             option2 += 1.0;
           });
         }
-        if (choice == 3) {
+        if (choice == 2) {
           setState(() {
             option3 += 1.0;
           });
         }
-        if (choice == 4) {
+        if (choice == 3) {
           setState(() {
             option4 += 1.0;
           });
