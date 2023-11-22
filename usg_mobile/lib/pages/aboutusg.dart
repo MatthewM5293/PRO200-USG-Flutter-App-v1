8390a12d16d470fdf00b347748fcdeaaa361112b
RileySorensen <98629467+RileySorensen@users.noreply.github.com>
2023-11-21 19:22:53 -0700

    Formating AboutUSG

--- usg_mobile/lib/pages/aboutusg.dart
+++ usg_mobile/lib/pages/aboutusg.dart
@@ -1,26 +1,42 @@
-
-
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class AboutUsg extends StatelessWidget
-{
+
+class AboutUsg extends StatelessWidget {
   const AboutUsg();
-  Widget build(BuildContext context)
-  {
+
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("USG"),
-      ),body: Center(
-        child:Row(
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
+        title: Text("About USG"),
+      ), body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/images/NeumontLogo.png',height: 100,width: 100),
-            Text("USG")
+            Row(
+              children: [
+                Column(
+                  children: [
+                    const Padding(padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0)),
+                    Image.asset('assets/images/NeumontLogo.png', height: 100, width: 100),
+                  ],
+                ),
+                const Column(
+                  children: [
+                    Padding(padding: EdgeInsetsDirectional.fromSTEB(0, 25, 50, 0)),
+                    Text("USG",style: TextStyle(fontSize: 30,fontWeight:FontWeight.bold),)
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
-    ),
+      ),
     );
   }
 }
