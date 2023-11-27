dcf742e675fae6fde4948d3a707cc7bb54f20ddc
RileySorensen <98629467+RileySorensen@users.noreply.github.com>
2023-11-27 09:47:18 -0700

    Completed About USG page

--- usg_mobile/lib/pages/aboutusg.dart
+++ usg_mobile/lib/pages/aboutusg.dart
@@ -9,35 +9,95 @@ class AboutUsg extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("About USG"),
-      ), body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+      ),
+      body: Center(
+        child: Column(
           children: [
-            Row(
+            Container(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Column(
-                  children: [
-                    const Padding(padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0)),
-                    Image.asset('assets/images/NeumontLogo.png', height: 100, width: 100),
-                  ],
-                ),
+                Column(children: [
+                  const Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0)),
+                  Image.asset(
+                    'assets/images/NeumontLogo.png',
+                    height: 100,
+                    width: 100,
+                  )
+                ]),
                 const Column(
                   children: [
-                    Padding(padding: EdgeInsetsDirectional.fromSTEB(0, 25, 50, 0)),
-                    Text("USG",style: TextStyle(fontSize: 30,fontWeight:FontWeight.bold),)
+                    Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0)),
+                    Text(
+                      "USG",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    )
                   ],
-                ),
+                )
               ],
-            ),
+            )),
+            Container(
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10)),
+                  Expanded(
+                      child: Text(
+                          '   USG, or Unified Student Government is what brings a voice to the student body, and '
+                          'this app is meant to help streamline the connection between us and you.')
+                  ),
+                  Padding(padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10)),
+                ],
+              ),
+            )
           ],
         ),
+        // child: Row(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: [
+        //     Row(
+        //       children: [
+        //         Column(
+        //           children: [
+        //             const Padding(
+        //                 padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0)),
+        //             Image.asset('assets/images/NeumontLogo.png',
+        //                 height: 100, width: 100),
+        //           ],
+        //         ),
+        //         const Column(
+        //           children: [
+        //             Padding(
+        //                 padding: EdgeInsetsDirectional.fromSTEB(0, 25, 0, 0)),
+        //             Text(
+        //               "USG",
+        //               style:
+        //                   TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        //             )
+        //           ],
+        //         ),
+        //       ],
+        //     ),
+        //     const Row(
+        //       mainAxisAlignment: MainAxisAlignment.center,
+        //       children: [
+        //         Column(children: [
+        //           Padding(padding: EdgeInsetsDirectional.fromSTEB(0, 25, 0, 0)),
+        //           Text(
+        //             'Testing',
+        //             textAlign: TextAlign.center,
+        //           )
+        //         ])
+        //       ],
+        //     )
+        //   ],
+        // ),
       ),
     );
   }
 }
-
